58c45d7f0d9767d0c75d7b1b27c012ccc84b2f8e
Ashlyn Hanson <44590338+AshlynHanson@users.noreply.github.com>
2019-09-30 21:35:41 -0500

    Update Rock.java

--- Rock.java
+++ Rock.java
@@ -1,5 +1,6 @@
-//creates a Rock object in a location on the grid
-
+/**
+creates a Rock object in a location on the grid
+*/
 public class Rock {
 	private Location loc;
 	
@@ -15,6 +16,11 @@ public class Rock {
 		return "R";
 	}
 	
+	/**
+	Checks if the location of the rock is equal to the current location
+	@param loc the Location that is being checked against the Rock's current location
+	@return boolean wether location is equal or not
+	*/
 	public boolean isLocated(Location loc) {
 		if (this.loc.getRow() == loc.getRow() && this.loc.getCol() == loc.getCol()) {
 			return true;
